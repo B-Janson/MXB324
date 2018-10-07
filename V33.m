fe5950f223d8f77293c20950f388b362356c1779
Jackfenwick <43366586+Jackfenwick@users.noreply.github.com>
2018-10-07 17:36:42 +1000

    Still really broken

--- V33.m
+++ V33.m
@@ -2,11 +2,11 @@ function f = V33(DIM, i, h, h_old, phi, phi_old, k, k_old, t, PARAMS)
 % Sandstone/Rain
 
 n = DIM.n;
-dx = DIM.dx(1,1);
-dz = DIM.dz(1,1);
-K_xx = DIM.K_xx(1,1);
-K_zz = DIM.K_zz(1,1);
-VOL = DIM.VOL(1,1);
+D=DIM.DELTA(i,:);
+K_xx = DIM.SP(DIM.ST(i,2),2);
+K_zz = DIM.SP(DIM.ST(i,1),2);
+VOL = DIM.VOL(i,5);
+
 dt = PARAMS.dt;
 theta = PARAMS.theta;
 r_f = PARAMS.r_f;
@@ -14,7 +14,6 @@ active_flow = 1;
 active_flow_old = 1;
 
 point = (DIM.r == i);
-west = (DIM.r == i-1);
 south = (DIM.r == i-n);
 
 % if h(point) >= 0
@@ -28,11 +27,11 @@ south = (DIM.r == i-n);
 k_s = (k(point) + k(south)) / 2;
 k_s_old = (k_old(point) + k_old(south)) / 2;
 
-gamma_1 = -active_flow * r_f * dx / 2;
-gamma_2 = k_s * K_zz * ((h(south) - h(point)) / dz + 1) * dx / 2;
+gamma_1 = -active_flow * r_f * D(1) / 2;
+gamma_2 = k_s * K_zz * ((h(south) - h(point)) / D(3) + 1) * D(1) / 2;
 
-gamma_1_old = -active_flow_old * r_f * dx / 2;
-gamma_2_old = k_s_old * K_zz * ((h_old(south) - h_old(point)) / dz + 1) * dx / 2;
+gamma_1_old = -active_flow_old * r_f * D(1) / 2;
+gamma_2_old = k_s_old * K_zz * ((h_old(south) - h_old(point)) / D(3) + 1) * D(1) / 2;
 
 f = phi(point) - phi_old(point) + dt/VOL * (theta * ( ...
             gamma_1 + gamma_2) ...
